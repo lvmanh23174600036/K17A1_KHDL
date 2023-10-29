89256b189d53efc1fd7d1cb803feaf2c098d5f91
lvmanh23174600036 <148080518+lvmanh23174600036@users.noreply.github.com>
2023-10-29 23:36:27 +0700

    Add files via upload

---
+++ baitaptincoso.py
@@ -0,0 +1,30 @@
+
+print("bài tập 1.1")
+print("**    **    ********    **        **        ******")
+print("**    **    **          **        **        **  **")
+print("********    ********    **        **        **  **")  
+print("**    **    **          **        **        **  **")
+print("**    **    ********    *******   *******   ******")
+print("bài tập 1.2") 
+x=10
+y=5
+c=x+y
+d=x-y
+e=x*y
+f=x/y
+print('Tổng của' , x, '+', y, '=', c)
+print('Hiệu của', x, '-', y, '=', d)
+print("tích của", x,"x",y, "=",e )
+print("Thương của",x,"/",y,"=",f )
+
+print("bài tập 1.3")
+ten_hang="sữa hộp Vina milk"
+so_luong=5
+don_gia=25000
+tien_phai_tra=so_luong*don_gia
+print("tên hàng:",ten_hang)
+print("số lượng:",so_luong)
+print("đơn giá:",don_gia)
+tien_phai_tra = so_luong*don_gia
+print("tiền phải trả:",tien_phai_tra,"vnđ")
+
